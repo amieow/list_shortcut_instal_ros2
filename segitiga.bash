966962eae11108cdf94ea3ddafeaf005bd4bd061
Fauzan Firdaus <109803246+amieow@users.noreply.github.com>
2026-09-12 12:58:39 +0700

    Add turtle control commands to segitiga.bash
    Add commands to control turtle in turtlesim.

---
+++ segitiga.bash
@@ -0,0 +1,7 @@
+
+ros2 action send_goal /turtle1/rotate_absolute turtlesim/action/RotateAbsolute "{theta: 0.0}"
+ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
+
+ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
+
+ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
