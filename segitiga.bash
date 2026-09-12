093b9ca613f28687c46394e346f23576e2bb7047
Fauzan Firdaus <109803246+amieow@users.noreply.github.com>
2026-09-12 13:08:30 +0700

    Add rotation actions to segitiga.bash script

--- segitiga.bash
+++ segitiga.bash
@@ -2,6 +2,8 @@
 ros2 action send_goal /turtle1/rotate_absolute turtlesim/action/RotateAbsolute "{theta: 0.0}"
 ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
 
+ros2 action send_goal /turtle1/rotate_absolute turtlesim/action/RotateAbsolute "{theta: 2.0944}"
 ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
 
+ros2 action send_goal /turtle1/rotate_absolute turtlesim/action/RotateAbsolute "{theta: -2.0944}"
 ros2 service call /turtle1/teleport_relative turtlesim/srv/TeleportRelative "{linear: 3.0, angular: 0.0}"
